0d7cc41c1f4461e4efcc9c2fb94737e39ec0b1e5
unknown <jtc7537@gmail.com>
2018-03-09 22:46:42 -0500

    added basic modal

--- redux/api/api-data.js
+++ redux/api/api-data.js
@@ -1,7 +1,7 @@
 var data = [
     { 
         name: 'asdf',
-        image: 'asdf',
+        image: 'http://wptest.io/demo/wp-content/uploads/sites/2/2012/12/unicorn-wallpaper.jpg',
         scores: [ '1', '1', '1', '1', '1' ] 
     }
 ]
